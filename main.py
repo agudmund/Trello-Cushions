edd72ec71a6e1a57ff8bdafca2b616d767d0f920
Ævar Guðmundsson <aevar.gudmundsson@gmail.com>
2026-02-10 20:47:53 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ from PySide6.QtWidgets import QApplication
 from main_window import TrelloCushionsWindow
 from utils.logging import log_message
 
-log_message("TRELLO CUSHIONS LAUNCHED")
-log_message("Made with one shared braincell 🧠")
+log_message("Trello Cushions")
 
 if __name__ == "__main__":
     try:
@@ -18,7 +17,6 @@ if __name__ == "__main__":
         window.show()
         sys.exit(app.exec())
     except Exception as e:
-        log_message(f"Startup catastrophically failed: {str(e)}", level="CRITICAL")  # ← we can make logging support levels later if we want
-        # Optional extra drama:
+        log_message(f"Startup catastrophically failed: {str(e)}", level="CRITICAL")
         print(f"Trello Cushions has entered the void: {str(e)}", file=sys.stderr)
         sys.exit(1)
